d5d29d1eb6fa9c8e6bfe46cdecf849b44ef742a8
ssurgutsky <ssurgutsky@gmail.com>
2020-04-01 13:45:15 +0700

    - init from ma1

---
+++ build_release.bat
@@ -0,0 +1,4 @@
+cordova build android --release -- --keystore=../keystore_krigames/krigames.keystore --storePassword=Aj109Asd291 --alias=krigames --password=Aj109Asd291
+
+xcopy platforms\android\app\build\outputs\apk\release\app-release.apk app-release.apk /y
+      
