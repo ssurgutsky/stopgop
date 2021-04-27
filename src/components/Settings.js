b75d4eb48ef1bd8502e8050c28273d1f8ebafb63
ssurgutsky <ssurgutsky@gmail.com>
2021-04-27 11:25:03 +0700

    - upd

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,9 +1,15 @@
 /* eslint no-eval: 0 */
 
 export default {
-  INDEXEDDB_VERSION: 5,
+  GAME_VERSION: 21,
+  INDEXEDDB_VERSION: 6,
   INDEXEDDB_STORE_NAME: 'store_sg',
   CACHE_ENABLED: true,
+  GAME_VERSION_VAR_NAME: 'game_version',
+  GAME_UPGRADE_POINT: 'GAME_UPGRADEPOINT',
+  GAME_SAVE_POINT: 'GAME_SAVEPOINT',
+  // Goto this mark if not found player's one on LOAD button pressing
+  DEFAULT_LOAD_MARK_NAME: 'START',
   GP_PRODUCT1_NAME: 'com.skystairgames.sg.coin1',
   GP_PRODUCT2_NAME: 'com.skystairgames.sg.episodes'
 }
